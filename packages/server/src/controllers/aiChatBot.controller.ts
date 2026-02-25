f8205548ab8117078bf1250e336f09d354df314b
Basir Naji <basirnaji007@gmail.com>
2026-02-26 02:39:37 +0430

    message history added

--- packages/server/src/controllers/aiChatBot.controller.ts
+++ packages/server/src/controllers/aiChatBot.controller.ts
@@ -1,5 +1,7 @@
 import type { Request, Response } from "express";
 import { Ollama } from "ollama";
+import z from "zod";
+import { conversationRepository } from "../../repositories/conversations.repository";
 
 // GET response from Ollama API
 const client = new Ollama({
@@ -7,27 +9,59 @@ const client = new Ollama({
   headers: { Authorization: "Bearer " + process.env.OLLAMA_API_KEY },
 });
 
+// schema validation
+const chatSchema = z.object({
+  prompt: z
+    .string()
+    .trim()
+    .min(1, "Prompt is required")
+    .max(1000, "Prompt is too long (max 1000 characters)"),
+  conversationId: z.uuid(),
+});
+
 // Controller function to handle chat requests
 export const getResponse = async (req: Request, res: Response) => {
   try {
-    const { prompt } = req.body as { prompt: string };
+    // parse request
+    const parseResult = chatSchema.safeParse(req.body);
 
-    if (!prompt) {
-      return res.status(400).json({ error: "Prompt is required" });
+    //error handling
+    if (!parseResult.success) {
+      res.status(400).json({ error: parseResult.error.issues });
+      return;
     }
+    const { prompt, conversationId } = parseResult.data;
+
+    // 1️⃣ get previous messages
+    const history = conversationRepository.getConversation(conversationId);
+    console.log(history);
+    // Create updated messages array
+    const updatedHistory = [
+      ...history,
+      { role: "user" as const, content: prompt },
+    ];
 
     const response = await client.chat({
       model: "gpt-oss:120b",
-      messages: [{ role: "user", content: prompt }],
+      // 2️⃣ call model with history
+      messages: updatedHistory,
       options: {
         temperature: 0.4,
       },
     });
 
-    console.log(response);
+    // Add assistant's response to history
+    const finalHistory = [
+      ...updatedHistory,
+      { role: "assistant" as const, content: response?.message?.content ?? "" },
+    ];
 
-    res.json({
-      reply: response?.message?.content ?? "No response returned",
+    // 3️⃣ Save updated history
+    conversationRepository.saveConversation(conversationId, finalHistory);
+
+    //model response
+    return res.json({
+      reply: response?.message?.content ?? "No response returned!",
     });
   } catch (error: any) {
     console.error("Ollama error:", error?.response?.data || error.message);
