873af0d844d04e9e0eb1fa87227cb9cac799b873
Basir Naji <basirnaji007@gmail.com>
2026-02-27 18:46:05 +0430

    chatbot and rendering responses

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,51 +1,16 @@
-import { useEffect, useState } from "react";
-import { Button } from "./components/ui/button";
-import { Input } from "./components/ui/input";
+import ChatBot from "./components/ChatBot";
+import Sidebar from "./components/Sidebar";
+import ThemeSwitcher from "./components/ThemeSwitcher";
 
-function App() {
-  const [message, setMessage] = useState("");
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
+const App = () => {
   return (
-    <div className="p-4">
-      <h1 className="bg-red-300">{message}</h1>
-      <Button>Click me</Button>
+    <div className="grid grid-cols-[auto_1fr] h-screen">
+      <Sidebar />
+      <div>
+        <ThemeSwitcher />
+        <ChatBot />
+      </div>
     </div>
   );
-}
-
+};
 export default App;
-
-// import { useState } from "react";
-
-// function App() {
-//   const [message, setMessage] = useState("");
-//   const [reply, setReply] = useState("");
-
-//   const sendMessage = async () => {
-//     const res = await fetch("http://localhost:3008/api/chat", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({ message }),
-//     });
-
-//     const data = await res.json();
-//     setReply(data.reply);
-//   };
-
-//   return (
-//     <div style={{ padding: 20 }}>
-//       <h1>Local Chatbot</h1>
-//       <Input value={message} onChange={(e) => setMessage(e.target.value)} />
-//       <Button onClick={sendMessage}>Send</Button>
-//       <p>{reply}</p>
-//     </div>
-//   );
-// }
-
-// export default App;
