ab99cc0be0ea1b7deeebd8ca239b6c63498f4301
Basir Naji <basirnaji007@gmail.com>
2026-04-10 21:59:43 +0430

    the icon colour change

--- packages/client/src/components/chat/Greeting.tsx
+++ packages/client/src/components/chat/Greeting.tsx
@@ -3,7 +3,7 @@ import { SiGooglegemini } from "react-icons/si";
 const Greeting = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full text-center text-gray-500 dark:text-gray-400 mt-20">
-      <SiGooglegemini size={48} className="mb-4 text-red-400" />
+      <SiGooglegemini size={48} className="mb-4 text-emerald-400" />
       <h2 className="text-2xl font-semibold mb-2">Hi, Basir </h2>
       <p>How I can help you?</p>
     </div>
